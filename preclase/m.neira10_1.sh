f14cd82529befd6b3843829ef1138f6f5bc7cf88
Mauricio Neira <m.neira10@uniandes.edu.co>
2018-11-16 07:42:51 -0500

    preclass torque

--- preclase/m.neira10_1.sh
+++ preclase/m.neira10_1.sh
@@ -2,12 +2,12 @@
 #PBS -l nodes=1:ppn=4,mem=1gb,walltime=00:10:00
 #PBS -M m.neira10@uniandes.edu.co
 #PBS -m abe
-#PBS -N mneira10_sample
+#PBS -N mneira10_integral
 #PBS -j oe
 
 
 module load anaconda/python3 
 module load gcc/4.9.4 
 module load openmpi/1.8.5
-cd /hpcfs/home/fisi4028/m.neira10/MauricioNeira_Ejercicio27/2nda_parte
+cd /hpcfs/home/fisi4028/m.neira10/MauricioNeira_Ejercicio28/preclase
 make
